23ca30677400b1c63fbf6f2321c0240b1ad92938
aoyesha <ayenamado26@gmail.com>
2026-01-30 17:06:24 +0800

    updated font

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:eccd/view/register_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:eccd/view/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -13,9 +15,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      theme: ThemeData(
+        textTheme: GoogleFonts.workSansTextTheme(),
+      ),
       debugShowCheckedModeBanner: false,
       title: 'ECD Checklist',
-      home: const RegisterPage(),
+      home: const LoginPage(),
     );
   }
 }
--- lib/util/navbar.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class NavItem {
-  final IconData icon;
-  final String label;
-  final VoidCallback onTap;
-
-  NavItem
-}
